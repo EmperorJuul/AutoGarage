e4dc41b26e18a9f918f87a8a24a2169cc075de95
EmperorJuul <juul.konings@novi-education.nl>
2022-09-28 12:12:17 +0200

    Eerste push

---
+++ src/main/java/nl/belastingdienst/autogarage/AutoGarageApplication.java
@@ -0,0 +1,13 @@
+package nl.belastingdienst.autogarage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoGarageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoGarageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/belastingdienst/autogarage/AutoGarageApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.belastingdienst.autogarage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoGarageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
