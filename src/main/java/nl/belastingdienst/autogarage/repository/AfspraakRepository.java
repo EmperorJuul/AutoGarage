670eb1f2d22173f908a69b2c461e0e78ae3c20d9
EmperorJuul <juul.konings@novi-education.nl>
2022-09-30 13:39:56 +0200

    parameter gecorrigeerd

--- src/main/java/nl/belastingdienst/autogarage/repository/AfspraakRepository.java
+++ src/main/java/nl/belastingdienst/autogarage/repository/AfspraakRepository.java
@@ -1,6 +1,7 @@
 package nl.belastingdienst.autogarage.repository;
 
+import nl.belastingdienst.autogarage.model.Afspraak;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AfspraakRepository extends JpaRepository<AfspraakRepository, Long> {
+public interface AfspraakRepository extends JpaRepository<Afspraak, Long> {
 }
