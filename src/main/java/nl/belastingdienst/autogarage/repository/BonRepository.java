d903a04d8ce2b88ce6c5980e3086e9b0b52cb3a8
EmperorJuul <juul.konings@novi-education.nl>
2022-10-06 11:09:19 +0200

    Entity verwijderd ivm feedback

--- src/main/java/nl/belastingdienst/autogarage/repository/BonRepository.java
+++
@@ -1,7 +0,0 @@
-package nl.belastingdienst.autogarage.repository;
-
-import nl.belastingdienst.autogarage.model.Bon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BonRepository extends JpaRepository<Bon, Long> {
-}
