b90558b1f87eed1e44ad21029dd8b2c6952f4810
EmperorJuul <juul.konings@novi-education.nl>
2022-10-06 11:40:42 +0200

    nieuwe data in dataloader

--- src/main/java/nl/belastingdienst/autogarage/dataloader/DataLoader.java
+++ src/main/java/nl/belastingdienst/autogarage/dataloader/DataLoader.java
@@ -5,6 +5,7 @@ import nl.belastingdienst.autogarage.repository.*;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Component
 public class DataLoader {
@@ -25,20 +26,30 @@ public class DataLoader {
     }
 
     public void load(){
-        Klant klant1 = new Klant("Juul", "Konings", "0612345678", "j.konings@belastingdienst.nl");
-        Klant klant2 = new Klant("Pieter", "Hogeboboom", "0687654321", "PH@hotmail.com");
-        klantRepository.save(klant1);
-        klantRepository.save(klant2);
+        Afspraak afspraak1 = new Afspraak(LocalDateTime.of(2020,10,6,10,00), LocalDateTime.of(2020, 10,6,11,00));
+        Afspraak afspraak2 = new Afspraak(LocalDateTime.of(2020,10,6,11,30), LocalDateTime.of(2020,10,6,12,00));
+        afspraakRepository.save(afspraak1);
+        afspraakRepository.save(afspraak2);
 
         Auto auto1 = new Auto("Opel", "Corsa", 2006, "DF-45-A4");
         Auto auto2 = new Auto("Volkswagen", "ID4", 2021, "23-HC-6G");
         autoRepository.save(auto1);
         autoRepository.save(auto2);
 
+        Klant klant1 = new Klant("Juul", "Konings", "0612345678", "j.konings@belastingdienst.nl");
+        Klant klant2 = new Klant("Pieter", "Hogeboboom", "0687654321", "PH@hotmail.com");
+        klantRepository.save(klant1);
+        klantRepository.save(klant2);
+
         Onderdeel onderdeel1 = new Onderdeel("Band", "Michelin");
         Onderdeel onderdeel2 = new Onderdeel("Ruit", "Noordglas");
         onderdeelRepository.save(onderdeel1);
         onderdeelRepository.save(onderdeel2);
+
+        Reparatie reparatie1 = new Reparatie("Banden vervangen", 104);
+        Reparatie reparatie2 = new Reparatie("Olie vervangen", 90);
+        reparatieRepository.save(reparatie1);
+        reparatieRepository.save(reparatie2);
 
     }
 }
