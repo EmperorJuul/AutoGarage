7182319281b7bf9723414c5959781f7154203110
EmperorJuul <juul.konings@novi-education.nl>
2022-10-10 11:03:23 +0200

    toevoeging security

---
+++ src/main/java/nl/belastingdienst/autogarage/security/SecurityConfig.java
@@ -0,0 +1,6 @@
+package nl.belastingdienst.autogarage.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
